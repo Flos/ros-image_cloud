f460efebec293daa63159dfbe80b2e516bb8aa96
Florian Nolden <fn187-github@yahoo.de>
2015-01-07 13:54:01 +0100

    pcl_to_image added dynamic reconfigure

--- include/image_cloud/calibration/pcl_to_image_nodelet.h
+++ include/image_cloud/calibration/pcl_to_image_nodelet.h
@@ -27,6 +27,11 @@
 #include <pcl/conversions.h>
 #include <pcl/filters/frustum_culling.h>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/range_image/range_image_planar.h>
+#include <pcl/features/range_image_border_extractor.h>
+#include <pcl/visualization/range_image_visualizer.h>
+
 #include <cv_bridge/cv_bridge.h>
 #include <opencv/cv.h>
 #include <eigen3/Eigen/Core>
@@ -40,6 +45,10 @@
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <pcl/keypoints/harris_3d.h>
+#include <dynamic_reconfigure/server.h>
+#include <image_cloud/pcl_imageConfig.h>
+
 
 namespace image_cloud {
 
@@ -49,35 +58,44 @@ typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::CameraInfo,
 
 
 class Pcl_to_image_nodelet : public nodelet::Nodelet {
+	typedef image_cloud::pcl_imageConfig Config;
+	typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
 public:
 	virtual void onInit();
 	virtual ~Pcl_to_image_nodelet();
 	virtual void callback(const sensor_msgs::CameraInfoConstPtr &input_msg_image_info, const PointCloud::ConstPtr &input_msg_cloud);
+	virtual void reconfigure_callback(Config &config, uint32_t level);
+	virtual void init_params();
+	virtual void init_pubsub();
+	virtual void params();
+	virtual void extract_intensity(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, cv_bridge::CvImage& image_pcl, cv_bridge::CvImage& image_depth);
+	virtual void extract_normals(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, cv_bridge::CvImage& image_pcl, cv_bridge::CvImage& image_depth);
+	virtual void extract_intensity_and_normals(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, cv_bridge::CvImage& image_pcl, cv_bridge::CvImage& image_depth);
+	pcl::PointCloud<pcl::PointXYZINormal>::Ptr calculate_normals(
+			const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud);
 
 private:
-	message_filters::Subscriber<sensor_msgs::CameraInfo>* image_info_sub;
-	message_filters::Subscriber<PointCloud>* pointcloud_sub;
-	message_filters::Synchronizer<Image_to_cloud_sync>* sync;
-	ros::Subscriber sub_;
+	boost::shared_ptr<ReconfigureServer> reconfigure_server_;
+	boost::shared_ptr<message_filters::Subscriber<sensor_msgs::CameraInfo> > image_info_sub;
+	boost::shared_ptr<message_filters::Subscriber<PointCloud> > pointcloud_sub;
+	boost::shared_ptr<message_filters::Synchronizer<Image_to_cloud_sync> > sync;
 
-	image_transport::ImageTransport *it_;
+
+	boost::shared_ptr<image_transport::ImageTransport> it_;
+	ros::Subscriber sub_;
 	image_transport::Publisher pub_;
+	image_transport::Publisher pub_depth_;
+	//image_transport::Publisher pub_range_;
 
 	std::string node_name_;
-	std::string subscribe_topic_pcl_;
-	std::string subscribe_topic_img_info_;
-	std::string filter_;
-	std::string image_frame_id_;
-	std::string reference_frame_id_;
-	std::string publish_image_topic_;
-	double resize_faktor_x_;
-	double resize_faktor_y_;
-
+	boost::mutex config_lock_;
+	Config config_;
 	ros::NodeHandle nh;
 
 	image_geometry::PinholeCameraModel camera_model;
 
 	tf::TransformListener listener_pointcloud_transform;
+
 
 };
 } /* end namespace */
--- src/calibration/pcl_to_image_nodelet.cpp
+++ src/calibration/pcl_to_image_nodelet.cpp
@@ -1,4 +1,4 @@
-#include "image_cloud/calibration/pcl_to_image_nodelet.h"
+#include "calibration/pcl_to_image_nodelet.h"
 #include <pluginlib/class_list_macros.h>
 
 // watch the capitalization carefully
@@ -6,58 +6,199 @@ PLUGINLIB_DECLARE_CLASS(image_cloud, Pcl_to_image_nodelet, image_cloud::Pcl_to_i
 
 namespace image_cloud {
 
+
 void
 Pcl_to_image_nodelet::onInit() {
 	NODELET_DEBUG("Initializing nodelet...");
 	nh = getPrivateNodeHandle();
-	nh.param<std::string>("name", node_name_, "image2pointcloud_nodelet");
-	nh.param<std::string>("sub_pcl", subscribe_topic_pcl_, "");
-	nh.param<std::string>("sub_img_info", subscribe_topic_img_info_, "");
 
-	// Optinal
-	nh.param<std::string>("pub_img", publish_image_topic_, subscribe_topic_pcl_ + "_mono8");
-	nh.param<std::string>("image_frame_id", image_frame_id_, "");
-	nh.param<double>("resize_x", resize_faktor_x_, 1);
-	nh.param<double>("resize_y", resize_faktor_y_, 1);
+	init_params();
 
-	// 2. Info
-	ROS_INFO_NAMED(node_name_, "name:\t\t%s", node_name_.c_str());
-	ROS_INFO_NAMED(node_name_, "sub_pcl: \t%s", subscribe_topic_pcl_.c_str());
-	ROS_INFO_NAMED(node_name_, "sub_img_info: \t%s", subscribe_topic_img_info_.c_str());
-	// Optional
-	ROS_INFO_NAMED(node_name_, "pub_img: \t%s:", publish_image_topic_.c_str());
-	ROS_INFO_NAMED(node_name_, "resize_width:\t%f", resize_faktor_x_);
-	ROS_INFO_NAMED(node_name_, "resize_height:\t%f", resize_faktor_y_);
-	ROS_INFO_NAMED(node_name_, "opt, image_frame_id:\t%s", image_frame_id_.c_str());
+	it_.reset( new image_transport::ImageTransport(nh));
+	init_pubsub();
 
-	if(subscribe_topic_img_info_.empty()) {
-		ROS_ERROR_NAMED(node_name_, "no camera_info subscribe topic defined");
-		return;
-	}
 
-	if(subscribe_topic_pcl_.empty()) {
-		ROS_ERROR_NAMED(node_name_, "no pcl subscribe topic defined");
-		return;
-	}
-
-	//Filter messages
-	int queue_size = 30;
-	image_info_sub = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh, subscribe_topic_img_info_, queue_size);
-	pointcloud_sub = new message_filters::Subscriber<PointCloud>(nh, subscribe_topic_pcl_, queue_size);
-
-	// ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
-	sync = new message_filters::Synchronizer<Image_to_cloud_sync>(Image_to_cloud_sync(queue_size), *image_info_sub, *pointcloud_sub);
-	sync->registerCallback(boost::bind(&Pcl_to_image_nodelet::callback, this, _1, _2));
-
-	it_ = new image_transport::ImageTransport(nh);
-	pub_ = it_->advertise(publish_image_topic_.c_str(), 1);
+	// Set up dynamic reconfigure
+	reconfigure_server_.reset(new ReconfigureServer(nh));
+	ReconfigureServer::CallbackType f = boost::bind(&Pcl_to_image_nodelet::reconfigure_callback, this, _1, _2);
+	reconfigure_server_->setCallback(f);
 }
 
+void
+Pcl_to_image_nodelet::init_params(){
+	nh.param<std::string>("node_name", node_name_, "pcl_to_image");
+
+	nh.param<std::string>("subscribe_topic_pcl", config_.subscribe_topic_pcl, "");
+	nh.param<std::string>("subscribe_topic_img_info",  config_.subscribe_topic_img_info, "");
+	nh.param<std::string>("publish_topic", config_.publish_topic, "");
+	nh.param<std::string>("image_tf_frame_id", config_.image_tf_frame_id, "");
+
+	nh.param<bool>("use_reference", config_.use_reference, false);
+	nh.param<std::string>("reference_frame", config_.reference_frame, "");
+	nh.param<int>("point_size", config_.point_size, 1);
+	nh.param<double>("resize_faktor_x", config_.resize_faktor_x, 1);
+	nh.param<double>("resize_faktor_y", config_.resize_faktor_y, 1);
+
+	nh.param<int>("queue_size", config_.queue_size, 30);
+	nh.param<double>("normal_search_radius", config_.normal_search_radius, 0.2);
+	nh.param<int>("feature", config_.feature, 0);
+
+	params();
+}
+
+void
+Pcl_to_image_nodelet::params(){
+	// Info
+	ROS_INFO_NAMED(node_name_, "name:\t\t%s", node_name_.c_str());
+
+	ROS_INFO_NAMED(node_name_, "subscribe_topic_pcl:\t%s", config_.subscribe_topic_pcl.c_str());
+	ROS_INFO_NAMED(node_name_, "subscribe_topic_img_info:\t%s", config_.subscribe_topic_img_info.c_str());
+	ROS_INFO_NAMED(node_name_, "publish_topic: \t%s", config_.publish_topic.c_str());
+	ROS_INFO_NAMED(node_name_, "image_tf_frame_id:\t%s", config_.image_tf_frame_id.c_str());
+
+	ROS_INFO_NAMED(node_name_, "use_reference: \t %s", config_.use_reference, config_.use_reference ? "true" : "false" );
+	ROS_INFO_NAMED(node_name_, "reference_frame:\t%s:", config_.reference_frame.c_str());
+	ROS_INFO_NAMED(node_name_, "point_size:\t%i", config_.point_size);
+	ROS_INFO_NAMED(node_name_, "resize_faktor_x:\t%f", config_.resize_faktor_x);
+	ROS_INFO_NAMED(node_name_, "resize_faktor_y:\t%f", config_.resize_faktor_y);
+
+	ROS_INFO_NAMED(node_name_, "queue_size:\t%i", config_.queue_size);
+	ROS_INFO_NAMED(node_name_, "normal_search_radius:\t%f", config_.normal_search_radius);
+	ROS_INFO_NAMED(node_name_, "feature:\t%i", config_.feature);
+}
+
+void
+Pcl_to_image_nodelet::init_pubsub() {
+	ROS_INFO_NAMED(node_name_, "init_pubsub");
+	//Filter messages
+	image_info_sub.reset( new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh, config_.subscribe_topic_img_info, config_.queue_size));
+	pointcloud_sub.reset( new message_filters::Subscriber<PointCloud>(nh, config_.subscribe_topic_pcl, config_.queue_size));
+
+	// ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
+	sync.reset( new message_filters::Synchronizer<Image_to_cloud_sync>(Image_to_cloud_sync(config_.queue_size), *image_info_sub, *pointcloud_sub));
+	sync->registerCallback(boost::bind(&Pcl_to_image_nodelet::callback, this, _1, _2));
+
+	pub_ = it_->advertise(config_.publish_topic.c_str(), 1);
+	pub_depth_ = it_->advertise(config_.publish_topic + "_depth", 1);
+	//pub_range_ = it_->advertise(config_.subscribe_topic_pcl + "_depth", 1);
+}
+
+void
+Pcl_to_image_nodelet::reconfigure_callback(Config &config, uint32_t level){
+	ROS_INFO_NAMED(node_name_, "reconfiguration_callback");
+	config_lock_.lock();
+	bool reset = false;
+
+	if(config.subscribe_topic_pcl != config_.subscribe_topic_pcl
+		|| config.publish_topic != config_.publish_topic
+		|| config.subscribe_topic_img_info != config_.subscribe_topic_img_info
+		)
+	 {
+		reset = true;
+	 }
+
+	 config_ = config;
+	 params();
+
+	 if(reset){
+		 init_pubsub();
+	 }
+	 config_lock_.unlock();
+}
+
+void Pcl_to_image_nodelet::extract_intensity(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, cv_bridge::CvImage& image_pcl, cv_bridge::CvImage& image_depth) {
+	cv::Point2d point_image;
+	BOOST_FOREACH (const pcl::PointXYZI& pt, (*cloud).points){
+	// look up 3D position
+	// printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+	// project to 2D Image
+		if( pt.z > 1) { // min distance from camera 1m
+			point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+
+			if( ( point_image.x > 0
+							&& point_image.x < image_pcl.image.cols )
+					&& ( point_image.y > 0
+							&& point_image.y < image_pcl.image.rows )
+			)
+			{
+				// Get image Color
+				cv::circle(image_pcl.image, point_image, config_.point_size, cv::Scalar(pt.intensity), -1);
+				cv::circle(image_depth.image, point_image, config_.point_size, cv::Scalar(pt.z), -1);
+			}
+			// add colored pixel to cloud_color
+		}
+	}
+}
+
+void Pcl_to_image_nodelet::extract_normals(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, cv_bridge::CvImage& image_pcl, cv_bridge::CvImage& image_depth) {
+	pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud_normals = calculate_normals(cloud);
+	cv::Point2d point_image;
+	BOOST_FOREACH (const pcl::PointXYZINormal& pt, (*cloud_normals).points){
+	// look up 3D position
+	// printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+	// project to 2D Image
+		if( pt.z > 1) { // min distance from camera 1m
+			point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+
+			if( ( point_image.x > 0
+							&& point_image.x < image_pcl.image.cols )
+					&& ( point_image.y > 0
+							&& point_image.y < image_pcl.image.rows )
+			)
+			{
+				// Get image Color
+				cv::circle(image_pcl.image, point_image, config_.point_size, cv::Scalar(pt.data_n[0]/3 + pt.data_n[1]/3 + pt.data_n[2]/3 ), -1);
+				cv::circle(image_depth.image, point_image, config_.point_size, cv::Scalar(pt.z), -1);
+			}
+			// add colored pixel to cloud_color
+		}
+	}
+}
+
+pcl::PointCloud<pcl::PointXYZINormal>::Ptr Pcl_to_image_nodelet::calculate_normals(
+		const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud) {
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree_n(
+			new pcl::search::KdTree<pcl::PointXYZI>());
+	// Estimate the normals of the cloud_xyz
+	pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud_normals(
+			new pcl::PointCloud<pcl::PointXYZINormal>);
+	pcl::NormalEstimation<pcl::PointXYZI, pcl::PointXYZINormal> ne;
+	ne.setInputCloud(cloud);
+	ne.setSearchMethod(tree_n);
+	ne.setRadiusSearch(config_.normal_search_radius);
+	ne.compute(*cloud_normals);
+	return cloud_normals;
+}
+
+void Pcl_to_image_nodelet::extract_intensity_and_normals(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, cv_bridge::CvImage& image_pcl, cv_bridge::CvImage& image_depth) {
+	pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud_normals = calculate_normals(cloud);
+
+	cv::Point2d point_image;
+	BOOST_FOREACH (const pcl::PointXYZINormal& pt, (*cloud_normals).points){
+	// look up 3D position
+	// printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+	// project to 2D Image
+		if( pt.z > 1) { // min distance from camera 1m
+			point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+
+			if( ( point_image.x > 0
+							&& point_image.x < image_pcl.image.cols )
+					&& ( point_image.y > 0
+							&& point_image.y < image_pcl.image.rows )
+			)
+			{
+				// Get image Color
+				cv::circle(image_pcl.image, point_image, config_.point_size, cv::Scalar(pt.intensity/2 + (pt.data_n[0]/3 + pt.data_n[1]/3 + pt.data_n[2]/3)/2), -1);
+				cv::circle(image_depth.image, point_image, config_.point_size, cv::Scalar(pt.z), -1);
+			}
+			// add colored pixel to cloud_color
+		}
+	}
+}
 
 void
 Pcl_to_image_nodelet::callback(const sensor_msgs::CameraInfoConstPtr &input_msg_image_info, const PointCloud::ConstPtr &input_msg_cloud_ptr){
-	ROS_INFO_NAMED(node_name_,"callback");
-
+	NODELET_DEBUG("callback");
 	if( pub_.getNumSubscribers() == 0 ){ // dont do anything if no one is interessted
 		return;
 	}
@@ -67,87 +208,122 @@ Pcl_to_image_nodelet::callback(const sensor_msgs::CameraInfoConstPtr &input_msg_
 		return;
 	}
 
+	config_lock_.lock();
+
 	//Look up transform for cameraladybug_camera4
-
-
-	if(image_frame_id_.empty()){
-		image_frame_id_ = input_msg_image_info->header.frame_id;
+	if(config_.image_tf_frame_id.empty()){
+		config_.image_tf_frame_id = input_msg_image_info->header.frame_id;
 	}
 
 
-	listener_pointcloud_transform.waitForTransform(image_frame_id_.c_str(), //target frame
-			input_msg_cloud_ptr->header.frame_id.c_str(), //source frame
-			input_msg_image_info->header.stamp, // target time
-			ros::Duration(5.0)
-			);
+	if(!listener_pointcloud_transform.waitForTransform(config_.image_tf_frame_id.c_str(), //target frame
+														input_msg_cloud_ptr->header.frame_id.c_str(), //source frame
+														input_msg_image_info->header.stamp, // target time
+														ros::Duration(5.0)
+														)
+	)
+	{
+		NODELET_WARN("no valid transform from %s to %s", input_msg_cloud_ptr->header.frame_id.c_str(), config_.image_tf_frame_id.c_str() );
+		return;
+	}
 
-	ROS_INFO_NAMED(node_name_,"pointcloud2 w: %i \th: %i \t\ttime: %i.%i",input_msg_cloud_ptr->width, input_msg_cloud_ptr->height, input_msg_cloud_ptr->header.stamp.sec, input_msg_cloud_ptr->header.stamp.nsec );
+	NODELET_DEBUG("pointcloud2 w: %i \th: %i \t\ttime: %i.%i",input_msg_cloud_ptr->width, input_msg_cloud_ptr->height, input_msg_cloud_ptr->header.stamp.sec, input_msg_cloud_ptr->header.stamp.nsec );
 
 	pcl::PointCloud<pcl::PointXYZI> cloud;
-	pcl::fromROSMsg(*input_msg_cloud_ptr,cloud);
+	pcl::fromROSMsg(*input_msg_cloud_ptr, cloud);
 
-	// Transform to odom
-	// todo: Transform at pcl time to odom
-	if (!pcl_ros::transformPointCloud("/odom", cloud, cloud, listener_pointcloud_transform)) {
-			NODELET_WARN("Cannot transform point cloud to the fixed frame %s.", "/odom");
-	     return;
+	if(config_.use_reference && !config_.reference_frame.empty())
+	{
+		// Transform to odom
+		// todo: Transform at pcl time to odom
+		if (!pcl_ros::transformPointCloud( config_.reference_frame, cloud, cloud, listener_pointcloud_transform)) {
+				NODELET_WARN("Cannot transform point cloud to the fixed frame %s", config_.reference_frame.c_str());
+			 return;
+		}
 	}
 
 	// Todo: check if Transform at image time to image_frame_id is right
 	cloud.header.stamp = input_msg_image_info->header.stamp.toNSec();
 
 	// Transform to image_frame_id
-	if (!pcl_ros::transformPointCloud(image_frame_id_.c_str(), cloud, cloud, listener_pointcloud_transform)) {
-			NODELET_WARN("Cannot transform point cloud to the fixed frame %s.", image_frame_id_.c_str());
+	if (!pcl_ros::transformPointCloud(config_.image_tf_frame_id.c_str(), cloud, cloud, listener_pointcloud_transform)) {
+			NODELET_WARN("Cannot transform point cloud to the fixed frame %s", config_.image_tf_frame_id.c_str());
 		 return;
 	}
 
 	// If we are here we can start with the projection
 	sensor_msgs::CameraInfo cam_info(*input_msg_image_info);
-	cam_info.P[2] = resize_faktor_x_ * input_msg_image_info->P[2];	// Move the center of the projection the resized image centerx
-	cam_info.P[6] = resize_faktor_y_ * input_msg_image_info->P[6]; // Move the center of the projection the resized image centery
+	cam_info.P[2] = config_.resize_faktor_x * input_msg_image_info->P[2];	// Move the center of the projection the resized image centerx
+	cam_info.P[6] = config_.resize_faktor_y * input_msg_image_info->P[6]; // Move the center of the projection the resized image centery
 
 	camera_model.fromCameraInfo(cam_info);
 
-	ROS_INFO_NAMED(node_name_,"camera_model:  tx: %f, ty: %f, cx: %f, cy: %f, fx: %f, fy: %f ", camera_model.Tx(), camera_model.Ty(), camera_model.cx(), camera_model.cy(), camera_model.fx(), camera_model.fy() );
+	NODELET_DEBUG("camera_model:  tx: %f, ty: %f, cx: %f, cy: %f, fx: %f, fy: %f ", camera_model.Tx(), camera_model.Ty(), camera_model.cx(), camera_model.cy(), camera_model.fx(), camera_model.fy() );
 
-	cv::Point2d point_image;
 
 	cv::Mat image;
-
-	image = cv::Mat::zeros(input_msg_image_info->height*resize_faktor_y_, input_msg_image_info->width*resize_faktor_x_, CV_8UC1);
+	image = cv::Mat::zeros(input_msg_image_info->height*config_.resize_faktor_y, input_msg_image_info->width*config_.resize_faktor_x, CV_8UC1);
 	cv_bridge::CvImage image_pcl(input_msg_image_info->header, sensor_msgs::image_encodings::MONO8, image);
 
-   int i = 0;
-   BOOST_FOREACH (const pcl::PointXYZI& pt, cloud.points) {
-		// look up 3D position
-		// printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
-		// project to 2D Image
-	   if( pt.z > 1){ // min distance from camera 1m
-			point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+	cv::Mat mat_image_depth = cv::Mat::zeros(input_msg_image_info->height*config_.resize_faktor_y, input_msg_image_info->width*config_.resize_faktor_x, CV_8UC1);
+	cv_bridge::CvImage image_depth(input_msg_image_info->header, sensor_msgs::image_encodings::MONO8, mat_image_depth);
 
-			if( ( point_image.x  > 0
-				&&  point_image.x < image_pcl.image.cols )
-				&& ( point_image.y > 0
-				&&  point_image.y < image_pcl.image.rows )
-				)
-			{
-				// Get image Color
-				cv::circle(image_pcl.image, point_image, 1, cv::Scalar(pt.intensity));
-			}
-			// add colored pixel to cloud_color
-	   }
-   }
+	try{
+		switch(config_.feature){
+			default:
+			case 0: // Intensity
+				extract_intensity(cloud.makeShared(), image_pcl, image_depth);
+				break;
+			case 1: // Normals
+				extract_normals(cloud.makeShared(), image_pcl, image_depth);
+				break;
+			case 2: // Intesity + Normals
+				extract_intensity_and_normals(cloud.makeShared(), image_pcl, image_depth);
+				break;
+		}
+	}catch(std::exception &e){
+		NODELET_ERROR("Exception: %s", e.what());
+		return;
+	}
 
+	config_lock_.unlock();
    pub_.publish(image_pcl.toImageMsg());
-   ROS_INFO_NAMED(node_name_,"callback end");
+   pub_depth_.publish(image_depth.toImageMsg());
+
+   // range image
+   // Convert the cloud to range image.
+
+
+
+//   tf::Transform pose;
+//   Eigen::Affine3f sensorPose = Eigen::Affine3f(Eigen::Translation3f(cloud.sensor_origin_[0],
+//   								 cloud.sensor_origin_[1],
+//   								 cloud.sensor_origin_[2])) *
+//   								 Eigen::Affine3f(cloud.sensor_orientation_);
+//   	float noiseLevel = 0.0f, minimumRange = 0.0f;
+//   	pcl::RangeImagePlanar rangeImage;
+//   	rangeImage.createFromPointCloudWithFixedSize(cloud, image.rows, image.cols,
+//   			camera_model.cx(), camera_model.cy(),
+//   			camera_model.fx(), camera_model.fy(),
+//   			sensorPose, pcl::RangeImage::CAMERA_FRAME,
+//   			noiseLevel, minimumRange);
+
+//   	sensor_msgs::Image range_image;
+//   //	pcl::toROSMsg(*input_msg_cloud_ptr, range_image);
+//
+//
+//   	// Border extractor object.
+////   	pcl::RangeImageBorderExtractor borderExtractor(&rangeImage);
+////
+////   	borderExtractor.compute(*borders);
+// //  	rangeImage.getImagePoint()
+//
+//   //pub_range_.publish(range_image);
+
+   ROS_DEBUG_NAMED(node_name_,"callback end");
 }
 
 Pcl_to_image_nodelet::~Pcl_to_image_nodelet(){
-	delete image_info_sub;
-	delete pointcloud_sub;
-	delete it_;
-	delete sync;
 	}
 
 } /* end namespace */
