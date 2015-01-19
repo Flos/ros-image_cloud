cf75713ecca5d32654589649d3a1abcc3e053fa9
Florian Nolden <fn187-github@yahoo.de>
2015-01-19 03:48:19 +0100

    refactored small helpers

--- include/image_cloud/common/small_helpers.hpp
+++ include/image_cloud/common/small_helpers.hpp
@@ -1,14 +1,20 @@
 #ifndef SMALL_HELPER_H_
 #define SMALL_HELPER_H_
 
-inline bool inRange(float in, float range, float base ){
-	if(	(base + range) > in
-		&& in > ( base - range)) return true;
+template <class T>
+inline bool inRange(T in, T range, T base ){
+	if(	(base - range) < in && in < ( base + range) ) return true;
 	return false;
 }
 
 inline float point_distance(pcl::PointXYZI &point){
 	return sqrt((point.x* point.x)+(point.y* point.y)+(point.z* point.z));
+}
+
+template <class T>
+inline bool between(T min, T test, T max){
+	if( min < test && test < max ) return true;
+	return false;
 }
 
 #endif
