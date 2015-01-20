6825f828038f917ee1578c15d04eb704f6b71c65
Florian Nolden <fn187-github@yahoo.de>
2015-01-20 17:35:13 +0100

    clean up float arrays before use

--- include/image_cloud/gui/kitti/camera.h
+++ include/image_cloud/gui/kitti/camera.h
@@ -21,6 +21,7 @@
 
 //own
 #include <gui/kitti/serializable.h>
+#include <gui/kitti/tf.h>
 
 #ifndef INCLUDE_KITTI_CAMERA_H_
 #define INCLUDE_KITTI_CAMERA_H_
@@ -32,16 +33,18 @@ namespace kitti{
 class Camera : public Serializable{
 public:
 	int camera_nr;
-	float S[2];
-	float K[9];
-	float D[5];
-	float R[9];
-	float T[3];
-	float S_rect[2];
-	float R_rect[9];
-	float P_rect[12];
-	void get_camera_info(sensor_msgs::CameraInfo &info_msg, bool rect = true);
-	void set_camera_info(sensor_msgs::CameraInfo info_msg, bool rect = true);
+	float S[2]; // original image size
+	float K[9]; // calibration matrices (unrectified)
+	float D[5]; // distortion coefficients (unrectified)
+	float S_rect[2]; // image size after rectification
+	float P_rect[12]; // projection matrix after rectification
+
+	// rectifying rotation matrix
+	Tf tf; // rotation from camera 0 to camera i, translation from camera 0 to camera i
+	Tf tf_rect; // rotation from camera 0 to camera i, translation from camera 0 to camera i
+
+	void get_camera_info(sensor_msgs::CameraInfo &info_msg);
+	void set_camera_info(sensor_msgs::CameraInfo info_msg);
 
 	Camera();
 	Camera(int camera_nr);
--- src/gui/kitti/camera.cpp
+++ src/gui/kitti/camera.cpp
@@ -14,6 +14,18 @@ namespace kitti{
 Camera::Camera()
 {
 	camera_nr = 0;
+	set_zero(S, 2);
+	set_zero(K, 9);
+	set_zero(D, 5);
+
+	set_zero(tf.R, 9);
+	set_zero(tf.T, 3);
+
+	set_zero(S_rect, 2);
+	set_zero(tf_rect.R, 9);
+	set_zero(tf_rect.T, 3);
+
+	set_zero(P_rect, 12);
 }
 
 Camera::Camera(int camera_nr)
@@ -32,109 +44,53 @@ Camera::to_string(){
 	ss << "S_" <<  buffer; serialize_array(ss, S, 2);
 	ss << "K_" <<  buffer; serialize_array(ss, K, 9);
 	ss << "D_" <<  buffer; serialize_array(ss, D, 5);
-	ss << "R_" <<  buffer; serialize_array(ss, R, 9);
-	ss << "T_" <<  buffer; serialize_array(ss, T, 3);
+	ss << "R_" <<  buffer; serialize_array(ss, tf.R, 9);
+	ss << "T_" <<  buffer; serialize_array(ss, tf.T, 3);
 	ss << "S_rect_" <<  buffer; serialize_array(ss, S_rect, 2);
-	ss << "R_rect_" <<  buffer; serialize_array(ss, R_rect, 9);
+	ss << "R_rect_" <<  buffer; serialize_array(ss, tf_rect.R, 9);
 	ss << "P_rect_" <<  buffer; serialize_array(ss, P_rect, 12);
+
 	return ss.str();
 }
 
 void
-Camera::get_camera_info(sensor_msgs::CameraInfo &info_msg, bool rect){
-	info_msg.D[0] = D[0];
-	info_msg.D[1] = D[1];
-	info_msg.D[2] = D[2];
-	info_msg.D[3] = D[3];
-	info_msg.D[4] = D[4];
+Camera::get_camera_info(sensor_msgs::CameraInfo &info_msg){
 
-	info_msg.K[0] = K[0];
-	info_msg.K[1] = K[1];
-	info_msg.K[2] = K[2];
-	info_msg.K[3] = K[3];
-	info_msg.K[4] = K[4];
-	info_msg.K[5] = K[5];
-	info_msg.K[6] = K[6];
-	info_msg.K[7] = K[7];
-	info_msg.K[8] = K[8];
+	assert(info_msg.D.empty());
+	assert(info_msg.K.size() == 9);
+	assert(info_msg.P.size() == 12);
 
-	info_msg.P[0] = P_rect[0];
-	info_msg.P[1] = P_rect[1];
-	info_msg.P[2] = P_rect[2];
-	info_msg.P[3] = P_rect[3];
-	info_msg.P[4] = P_rect[4];
-	info_msg.P[5] = P_rect[5];
-	info_msg.P[6] = P_rect[6];
-	info_msg.P[7] = P_rect[7];
-	info_msg.P[8] = P_rect[8];
-	info_msg.P[9] = P_rect[9];
-	info_msg.P[10] = P_rect[10];
-	info_msg.P[11] = P_rect[11];
-
-	if(rect){
-		info_msg.R[0] = R_rect[0];
-		info_msg.R[1] = R_rect[1];
-		info_msg.R[2] = R_rect[2];
-		info_msg.R[3] = R_rect[3];
-		info_msg.R[4] = R_rect[4];
-		info_msg.R[5] = R_rect[5];
-		info_msg.R[6] = R_rect[6];
-		info_msg.R[7] = R_rect[7];
-		info_msg.R[8] = R_rect[8];
+	for(int i = 0; i < info_msg.D.size(); ++i){
+		info_msg.D.push_back(D[i]);
 	}
-	else{
-		info_msg.R[0] = R[0];
-		info_msg.R[1] = R[1];
-		info_msg.R[2] = R[2];
-		info_msg.R[3] = R[3];
-		info_msg.R[4] = R[4];
-		info_msg.R[5] = R[5];
-		info_msg.R[6] = R[6];
-		info_msg.R[7] = R[7];
-		info_msg.R[8] = R[8];
+
+	for(int i = 0; i < info_msg.K.size(); ++i){
+		info_msg.K[i] = K[i];
+	}
+
+	for(int i = 0; i < info_msg.P.size(); ++i){
+		info_msg.P[i] = P_rect[i];
 	}
 }
 
 void
-Camera::set_camera_info(sensor_msgs::CameraInfo info_msg, bool rect){
-	D[0] = info_msg.D[0];
-	D[1] = info_msg.D[1];
-	D[2] = info_msg.D[2];
-	D[3] = info_msg.D[3];
-	D[4] = info_msg.D[4];
+Camera::set_camera_info(sensor_msgs::CameraInfo info_msg){
 
-	K[0] = info_msg.K[0];
-	K[1] = info_msg.K[1];
-	K[2] = info_msg.K[2];
-	K[3] = info_msg.K[3];
-	K[4] = info_msg.K[4];
-	K[5] = info_msg.K[5];
-	K[6] = info_msg.K[6];
-	K[7] = info_msg.K[7];
-	K[8] = info_msg.K[8];
+	assert(info_msg.D.size() <= 5);
+	assert(info_msg.K.size() == 9);
+	assert(info_msg.P.size() == 12);
 
-	P_rect[0] = info_msg.P[0];
-	P_rect[1] = info_msg.P[1];
-	P_rect[2] = info_msg.P[2];
-	P_rect[3] = info_msg.P[3];
-	P_rect[4] = info_msg.P[4];
-	P_rect[5] = info_msg.P[5];
-	P_rect[6] = info_msg.P[6];
-	P_rect[7] = info_msg.P[7];
-	P_rect[8] = info_msg.P[8];
-	P_rect[9] = info_msg.P[9];
-	P_rect[11] = info_msg.P[10];
-	P_rect[11] = info_msg.P[11];
+	for(int i = 0; i < info_msg.D.size(); ++i){
+		D[i] = info_msg.D.at(i);
+	}
 
-	R[0] = info_msg.R[0];
-	R[1] = info_msg.R[1];
-	R[2] = info_msg.R[2];
-	R[3] = info_msg.R[3];
-	R[4] = info_msg.R[4];
-	R[5] = info_msg.R[5];
-	R[6] = info_msg.R[6];
-	R[7] = info_msg.R[7];
-	R[8] = info_msg.R[8];
+	for(int i = 0; i < info_msg.K.size(); ++i){
+		K[i] = info_msg.K[i];
+	}
+
+	for(int i = 0; i < info_msg.P.size(); ++i){
+		P_rect[i] = info_msg.P[i];
+	}
 }
 
 bool
@@ -166,13 +122,13 @@ Camera::load(std::istream& stream){
 		std::getline (stream, line);
 		in.clear(); in.str(line);
 		in >> type;
-		deserialize_array(in, R, 9);
+		deserialize_array(in, tf.R, 9);
 
 		// T
 		std::getline (stream, line);
 		in.clear(); in.str(line);
 		in >> type;
-		deserialize_array(in, T, 3);
+		deserialize_array(in, tf.T, 3);
 
 		// S_rect
 		std::getline (stream, line);
@@ -184,13 +140,16 @@ Camera::load(std::istream& stream){
 		std::getline (stream, line);
 		in.clear(); in.str(line);
 		in >> type;
-		deserialize_array(in, R_rect, 9);
+		deserialize_array(in, tf_rect.R, 9);
 
 		// P_rect
 		std::getline (stream, line);
 		in.clear(); in.str(line);
 		in >> type;
 		deserialize_array(in, P_rect, 12);
+
+		set_array(tf_rect.T, tf.T, 3);
+
 	}
 	catch(std::exception &e)
 	{
