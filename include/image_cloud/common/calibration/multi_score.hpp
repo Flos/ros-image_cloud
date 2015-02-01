896d632ef6967fcfe538d559e2c5d1d18f937d4b
Florian Nolden <fn187-github@yahoo.de>
2015-02-01 15:50:51 +0100

    added option to use deque and optional filtering

--- include/image_cloud/common/calibration/multi_score.hpp
+++ include/image_cloud/common/calibration/multi_score.hpp
@@ -26,10 +26,27 @@ multi_score(
 
 	for(int i = 0; i< idx.size(); ++i){
 		long unsigned score_temp;
-		objective_function(idx.at(i), edge_images.at(i),score_temp);
+		objective_function(idx.at(i), edge_images.at(i), score_temp);
 		score += score_temp;
 	}
 }
+
+template <typename PointT, typename ImageT>
+inline void
+multi_score(
+		std::deque<Projected_pointcloud<PointT> > &idx,
+		std::deque<cv::Mat> &edge_images,
+		long unsigned &score)
+{
+	assert(idx.size() == edge_images.size());
+
+	for(int i = 0; i< idx.size(); ++i){
+		long unsigned score_temp;
+		objective_function(idx.at(i), edge_images.at(i), score_temp);
+		score += score_temp;
+	}
+}
+
 
 /****
  * Transforms Pointclouds and calculates a result using the objective_function
@@ -52,15 +69,11 @@ multi_score(
 
 		image_cloud::transform_pointcloud<PointT>(pointclouds.at(i), transformed, search.x, search.y, search.z, search.roll, search.pitch, search.yaw);
 
-		Projected_pointcloud<PointT> p;
-
-		project2d::project_2d<PointT>( camera_model, transformed, p, edge_images.at(i).cols, edge_images.at(i).rows );
-		objective_function<PointT, ImageT>( p, edge_images.at(i), score_temp);
+		objective_function<PointT, ImageT>( camera_model, transformed, edge_images.at(i), score_temp);
 
 		search.result += score_temp;
 	}
 }
-
 
 /****
  * Transforms Pointclouds and calculates a result using the objective_function
@@ -83,10 +96,68 @@ multi_score(
 
 		image_cloud::transform_pointcloud<PointT>(pointclouds.at(i), transformed, search.x, search.y, search.z, search.roll, search.pitch, search.yaw);
 
-		Projected_pointcloud<PointT> p;
+		objective_function<PointT, ImageT>( camera_model, transformed, edge_images.at(i), score_temp);
 
-		project2d::project_2d<PointT>( camera_model, transformed, p, edge_images.at(i).cols, edge_images.at(i).rows );
-		objective_function<PointT, ImageT>( p, edge_images.at(i), score_temp);
+		search.result += score_temp;
+	}
+}
+
+
+
+/****
+ * Transforms Pointclouds and calculates a result using the objective_function
+ */
+template <typename PointT, typename ImageT>
+inline void
+multi_score_filter_depth(
+		const image_geometry::PinholeCameraModel &camera_model,
+		const std::vector<pcl::PointCloud<PointT> >& pointclouds,
+		const std::vector<cv::Mat> &edge_images,
+		search::Search_value &search
+		)
+{
+	assert(pointclouds.size() == edge_images.size());
+
+
+	for(int i = 0; i< pointclouds.size(); ++i){
+		long unsigned score_temp;
+		pcl::PointCloud<PointT> transformed, filtered;
+
+		image_cloud::transform_pointcloud<PointT>(pointclouds.at(i), transformed, search.x, search.y, search.z, search.roll, search.pitch, search.yaw);
+
+		filter::filter_depth_intensity( camera_model, transformed, filtered, edge_images.at(i).rows, edge_images.at(i).cols);
+
+		objective_function<PointT, ImageT>( camera_model, filtered, edge_images.at(i), score_temp);
+
+		search.result += score_temp;
+	}
+}
+
+
+/****
+ * Transforms Pointclouds and calculates a result using the objective_function
+ */
+template <typename PointT, typename ImageT>
+inline void
+multi_score_filter_depth(
+		const image_geometry::PinholeCameraModel &camera_model,
+		const std::deque<pcl::PointCloud<PointT> >& pointclouds,
+		const std::deque<cv::Mat> &edge_images,
+		search::Search_value &search
+		)
+{
+	assert(pointclouds.size() == edge_images.size());
+
+
+	for(int i = 0; i< pointclouds.size(); ++i){
+		long unsigned score_temp;
+		pcl::PointCloud<PointT> transformed, filtered;
+
+		image_cloud::transform_pointcloud<PointT>(pointclouds.at(i), transformed, search.x, search.y, search.z, search.roll, search.pitch, search.yaw);
+
+		filter::filter_depth_intensity<PointT>( camera_model, transformed, filtered, edge_images.at(i).rows, edge_images.at(i).cols);
+
+		objective_function<PointT, ImageT>( camera_model, filtered, edge_images.at(i), score_temp);
 
 		search.result += score_temp;
 	}
