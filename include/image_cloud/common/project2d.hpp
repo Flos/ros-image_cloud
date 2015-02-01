a724700f0f3363f17283da01485b753892d49cf7
Florian Nolden <fn187-github@yahoo.de>
2015-02-01 16:17:56 +0100

    function input is now const, remove boost loops

--- include/image_cloud/common/project2d.hpp
+++ include/image_cloud/common/project2d.hpp
@@ -21,11 +21,11 @@ project_2d(
 		unsigned int camera_height = 768
 		)
 {
-	BOOST_FOREACH (const pcl::PointXYZI& pt, in.points)
+	for(int i = 0; i < in.points.size(); ++i)
 	{
-		if( pt.z > 1) { // min distance from camera 1m
+		if( in.points.at(i).z > 1) { // min distance from camera 1m
 
-			cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+			cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(in.points.at(i).x, in.points.at(i).y, in.points.at(i).z));
 
 			if( between<int>(0, point_image.x, camera_width )
 				&& between<int>( 0, point_image.y, camera_height )
@@ -36,7 +36,7 @@ project_2d(
 				pt_2d.x = point_image.x;
 				pt_2d.y = point_image.y;
 				out_2d.push_back(pt_2d);
-				out_3d.push_back(pt);
+				out_3d.push_back(in.points.at(i));
 			}
 		}
 	}
@@ -61,11 +61,11 @@ project_2d(
 		float min_distance = 1
 		)
 {
-	BOOST_FOREACH (const PointT& pt, in.points)
+	for(int i = 0; i < in.points.size(); ++i)
 	{
-		if( pt.z > min_distance) { // min distance from camera 1m
+		if( in.points.at(i).z > min_distance) { // min distance from camera 1m
 
-			cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+			cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(in.points.at(i).x, in.points.at(i).y, in.points.at(i).z));
 
 			if( between<int>(0, point_image.x, image.cols )
 				&& between<int>( 0, point_image.y, image.rows )
@@ -74,10 +74,10 @@ project_2d(
 				switch(field){
 					default:
 					case DEPTH:
-							cv::circle(image, point_image, point_size, cv::Scalar(pt.z), -1);
+							cv::circle(image, point_image, point_size, cv::Scalar(in.points.at(i).z), -1);
 						break;
 					case INTENSITY:
-							cv::circle(image, point_image, point_size, cv::Scalar(pt.intensity), -1);
+							cv::circle(image, point_image, point_size, cv::Scalar(in.points.at(i).intensity), -1);
 						break;
 				}
 			}
@@ -89,114 +89,116 @@ template <typename PointT>
 inline void
 project_2d(
 		const image_geometry::PinholeCameraModel &camera_model,
-		pcl::PointCloud<PointT> &in,
-		std::vector<std::vector<boost::shared_ptr<pcl::PointXYZI> > > &out,
-		int image_width,
-		int image_height)
+		const pcl::PointCloud<PointT> &in,
+		std::vector<std::vector<boost::shared_ptr<PointT> > > &out
+		)
 {
-		BOOST_FOREACH (pcl::PointXYZI& pt, in.points){
-			if( pt.z > 1) { // min distance from camera 1m
+	for(int i = 0; i < in.points.size(); ++i)
+	{
+		if( in.points.at(i).z > 1) { // min distance from camera 1m
 
-				cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+			cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(in.points.at(i).x, in.points.at(i).y, in.points.at(i).z));
 
-				if( between<int>(0, point_image.x, image_width )
-					&& between<int>( 0, point_image.y, image_height )
-				)
-				{
-					// Point in image push to 2d and 3d point
-					out[point_image.x][point_image.y].reset(new pcl::PointXYZI(pt));
-				}
+			if( between<int>(0, point_image.x, out.size() )
+				&& between<int>( 0, point_image.y, out[0].size() )
+			)
+			{
+				// Point in image push to 2d and 3d point
+				out[point_image.x][point_image.y].reset(new PointT(in.points.at(i)));
 			}
 		}
+	}
 }
 
 template <typename PointT>
 inline void
 project_2d(
 		const image_geometry::PinholeCameraModel &camera_model,
-		pcl::PointCloud<PointT> &in,
-		std::vector<std::vector<boost::shared_ptr<pcl::PointXYZI> > > &out_vector,
+		const pcl::PointCloud<PointT> &in,
+		std::vector<std::vector<boost::shared_ptr<PointT> > > &out_vector,
 		Projected_pointcloud<PointT> &out,
 		int image_width,
 		int image_height)
 {
-		assert(out_vector.size() == image_width);
-		assert(out_vector[0].size() == image_height);
-		out.image_size.width = image_width;
-		out.image_size.height = image_height;
+	assert(out_vector.size() == image_width);
+	assert(out_vector[0].size() == image_height);
+	out.image_size.width = image_width;
+	out.image_size.height = image_height;
 
-		BOOST_FOREACH (pcl::PointXYZI& pt, in.points){
-			if( pt.z > 1) { // min distance from camera 1m
+	for(int i = 0; i < in.points.size(); ++i)
+	{
+		if( in.points.at(i).z > 1) { // min distance from camera 1m
 
-				cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+			cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(in.points.at(i).x, in.points.at(i).y, in.points.at(i).z));
 
-				if( between<int>(0, point_image.x, image_width )
-					&& between<int>( 0, point_image.y, image_height )
-				)
-				{
-					// Point in image push to 2d and 3d point
-					out_vector[point_image.x][point_image.y].reset(new pcl::PointXYZI(pt));
-					out.points.push_back(Projected_Point<PointT>(pt, point_image));
-				}
+			if( between<int>(0, point_image.x, image_width )
+				&& between<int>( 0, point_image.y, image_height )
+			)
+			{
+				// Point in image push to 2d and 3d point
+				out_vector[point_image.x][point_image.y].reset(new PointT(in.points.at(i)));
+				out.points.push_back(Projected_Point<PointT>(in.points.at(i), point_image));
 			}
 		}
+	}
 }
 
 template <typename PointT>
 inline void
 project_2d(
 		const image_geometry::PinholeCameraModel &camera_model,
-		pcl::PointCloud<PointT> &in,
+		const pcl::PointCloud<PointT> &in,
 		Projected_pointcloud<PointT> &out,
 		int image_width,
 		int image_height)
 {
-		out.image_size.width = image_width;
-		out.image_size.height = image_height;
+	out.image_size.width = image_width;
+	out.image_size.height = image_height;
 
-		BOOST_FOREACH (pcl::PointXYZI& pt, in.points){
-			if( pt.z > 1) { // min distance from camera 1m
+	for(int i = 0; i < in.points.size(); ++i)
+	{
+		if( in.points.at(i).z > 1) { // min distance from camera 1m
 
-				cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(pt.x, pt.y, pt.z));
+			cv::Point2i point_image = camera_model.project3dToPixel(cv::Point3d(in.points.at(i).x, in.points.at(i).y, in.points.at(i).z));
 
-				if( between<int>(0, point_image.x, image_width )
-					&& between<int>( 0, point_image.y, image_height )
-				)
-				{
-					out.points.push_back(Projected_Point<PointT>(pt, point_image));
-				}
+			if( between<int>(0, point_image.x, image_width )
+				&& between<int>( 0, point_image.y, image_height )
+			)
+			{
+				out.points.push_back(Projected_Point<PointT>(in.points.at(i), point_image));
 			}
 		}
+	}
 }
 
 
 template <typename PointT>
 inline void
 project_2d(
-		std::vector<std::vector<boost::shared_ptr<PointT> > > &in,
+		const std::vector<std::vector<boost::shared_ptr<PointT> > > &in,
 		cv::Mat &out,
 		Field field = INTENSITY,
 		int point_size = 1
 		)
 {
-		for(int y = 0; y < in[0].size(); y++)
+	for(int y = 0; y < in[0].size(); y++)
+	{
+		for(int x = 0; x < in.size(); x++)
 		{
-			for(int x = 0; x < in.size(); x++)
-			{
-				cv::Point3i value;
-				if( in[x][y]){ /* found something */
-					switch(field){
-						default:
-						case DEPTH:
-								cv::circle(out, cv::Point2i(x,y), point_size, cv::Scalar(in[x][y]->z), -1);
-							break;
-						case INTENSITY:
-								cv::circle(out, cv::Point2i(x,y), point_size, cv::Scalar(in[x][y]->intensity), -1);
-							break;
-					}
+			cv::Point3i value;
+			if( in[x][y]){ /* found something */
+				switch(field){
+					default:
+					case DEPTH:
+							cv::circle(out, cv::Point2i(x,y), point_size, cv::Scalar(in[x][y]->z), -1);
+						break;
+					case INTENSITY:
+							cv::circle(out, cv::Point2i(x,y), point_size, cv::Scalar(in[x][y]->intensity), -1);
+						break;
 				}
 			}
 		}
+	}
 }
 
 }
