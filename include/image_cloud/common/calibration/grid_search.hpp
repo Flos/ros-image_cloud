b37eead55e7b8b329b95f28046c14cc19d94246b
Florian Nolden <fn187-github@yahoo.de>
2015-02-21 13:10:30 +0100

    added sorting of results and debug output

--- include/image_cloud/common/calibration/grid_search.hpp
+++ include/image_cloud/common/calibration/grid_search.hpp
@@ -4,6 +4,7 @@
 #include <image_cloud/common/calibration/structs.hpp>
 
 #include <deque>
+#include <algorithm>
 
 #include <opencv2/core/core.hpp>
 #include <math.h>
@@ -11,9 +12,11 @@
 #ifndef SEARCH_GRID_6D_H_
 #define SEARCH_GRID_6D_H_
 
+//#define debug
 namespace search
 {
 	inline void grid_setup(Search_setup setup, std::vector<Search_value>& results){
+		results.push_back(Search_value(setup.x.center, setup.y.center, setup.z.center, setup.roll.center, setup.pitch.center, setup.yaw.center)); // first value is origin
 		for(int x=0; x < setup.x.steps_max; ++x)
 		{
 			for(int y=0; y < setup.y.steps_max; ++y)
@@ -26,13 +29,33 @@ namespace search
 						{
 							for(int yaw=0; yaw < setup.yaw.steps_max; ++yaw)
 							{
+								// Origin is always the first element, dont add it again
+								if(setup.x.center == setup.x.at(x)
+										&& setup.y.center == setup.y.at(y)
+										&& setup.z.center == setup.z.at(z)
+										&& setup.roll.center == setup.roll.at(roll)
+										&& setup.pitch.center == setup.pitch.at(pitch)
+										&& setup.yaw.center == setup.yaw.at(yaw))
+								{
+#ifdef debug
+									std::cout << results.size()-1 << "\t" << x << y << z<< roll<< pitch << yaw << std::endl;
+#endif
+									continue;
+								}
+
 								results.push_back(Search_value(setup.x.at(x), setup.y.at(y), setup.z.at(z), setup.roll.at(roll), setup.pitch.at(pitch), setup.yaw.at(yaw)));
+#ifdef debug
+								std::cout << results.size()-1 << "\t" << results.at(results.size()-1).to_simple_string() << std::endl;
+#endif
 							}
 						}
 					}
 				}
 			}
 		}
+#ifdef debug
+		std::cout << "grid_setup: " << results.size();
+#endif
 	}
 
 	template <typename PointT, typename ImageT>
@@ -62,6 +85,53 @@ namespace search
 		}
 	}
 
+	inline bool sortByscore(const Search_value &lhs, const Search_value &rhs) { return lhs.score > rhs.score; }
+
+	inline void evaluate_results(std::vector<Search_value> &result_list,
+			tf::Transform& out,
+			Multi_search_result* multi_result = NULL)
+	{
+		unsigned long int worse = 0;
+		unsigned long int best_result = 0;
+		unsigned long int best_result_idx = 0;
+		for (int i = 1; i < result_list.size(); ++i) {
+			if (result_list.at(i).score > best_result) {
+				best_result_idx = i;
+				best_result = result_list.at(i).score;
+			}
+			if (result_list.at(0).score > result_list.at(i).score) {
+				++worse;
+			}
+		}
+		result_list.at(best_result_idx).get_transform(out);
+		if (multi_result != NULL) {
+			multi_result->best = result_list.at(best_result_idx);
+			multi_result->center = result_list.at(0);
+			multi_result->nr_worse = worse;
+			multi_result->nr_total = result_list.size();
+			multi_result->best_results.assign(result_list.begin(), result_list.end());
+			//Sort results by score
+			std::sort(multi_result->best_results.begin(), multi_result->best_results.end(), sortByscore);
+
+		}
+}
+
+
+
+	template <typename PointT, typename ImageT>
+	inline void get_best_tf(std::vector<Search_value> result_list,
+						tf::Transform &out,
+						const image_geometry::PinholeCameraModel &camera_model,
+						const std::deque<pcl::PointCloud<PointT> > &pointclouds,
+						const std::deque<cv::Mat> &images,
+						bool pre_filtred = true,
+						Multi_search_result *multi_result = NULL)
+	{
+		calculate<PointT, ImageT>( camera_model, pointclouds, images, result_list, pre_filtred);
+
+		evaluate_results(result_list, out, multi_result);
+	}
+
 	template <typename PointT, typename ImageT>
 	inline void get_best_tf(	tf::Transform in,
 						tf::Transform &out,
@@ -89,51 +159,7 @@ namespace search
 
 		grid_setup(search_range, result_list);
 
-		Search_value origin_empty;
-		origin_empty.init(in);
-
-		// Add origin
-		if(result_list.size() %2 == 0){
-			result_list.insert(result_list.begin()+(result_list.size()/2), origin_empty);
-		}
-
-		calculate<PointT, ImageT>( camera_model, pointclouds, images, result_list, pre_filtred);
-
-		// get origin
-		Search_value origin = result_list.at( (result_list.size()) / 2);
-		std::cout << "center"<< spacer << origin.to_string() << "\n";
-		std::cout << "empty" << spacer << origin_empty.to_string() << "\n";
-//
-		assert(origin.x == origin_empty.x);
-		assert(origin.y == origin_empty.y);
-		assert(origin.z == origin_empty.z);
-		assert(origin.roll == origin_empty.roll);
-		assert(origin.pitch == origin_empty.pitch);
-		assert(origin.yaw == origin_empty.yaw);
-		assert(origin.result != origin_empty.result);
-
-		long unsigned int worse = 0;
-		long unsigned int best_result = 0;
-		long unsigned int best_result_idx = 0;
-
-		for(int i=0; i< result_list.size(); ++i){
-			if(result_list.at(i).result > best_result){
-				best_result_idx = i;
-				best_result = result_list.at(i).result;
-			}
-			if( origin.result > result_list.at(i).result ){
-				++worse;
-			}
-		}
-
-		result_list.at(best_result_idx).get_transform(out);
-
-		if(multi_result != NULL){
-			multi_result->best = result_list.at(best_result_idx);
-			multi_result->in = origin;
-			multi_result->nr_worse = worse;
-			multi_result->nr_total = result_list.size();
-		}
+		get_best_tf<PointT,ImageT>(result_list, out, camera_model, pointclouds, images, pre_filtred, multi_result);
 	}
 
 }
