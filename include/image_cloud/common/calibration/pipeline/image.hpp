4667a3f970c2e08e782f77d65080f35a57d9d2c7
Florian Nolden <fn187-github@yahoo.de>
2015-03-05 00:01:43 +0100

    added switches for edge and blur filters

--- include/image_cloud/common/calibration/pipeline/image.hpp
+++ include/image_cloud/common/calibration/pipeline/image.hpp
@@ -4,6 +4,8 @@
 #include <assert.h>
 #include <opencv2/opencv.hpp>
 
+#include <image_cloud/common/calibration/pipeline/enums.h>
+
 namespace image_cloud{
 	inline void convert_to_grey(const cv::Mat &in_unknown, cv::Mat &grey){
 		// Convert Greyscale
@@ -25,7 +27,7 @@ namespace image_cloud{
 	 * 	Edge Filter
 	 * 	Inverse distance transformation
 	 */
-	inline void create_inverse_transformed(const cv::Mat& in, cv::Mat &out){
+	inline void create_inverse_transformed(const cv::Mat& in, cv::Mat &out, image_filter::edge::Edge filter = image_filter::edge::MAX ){
 		cv::Mat grey,edge;
 
 		create_empty_grey_mat(edge, in.rows, in.cols);
@@ -35,10 +37,46 @@ namespace image_cloud{
 		convert_to_grey(in, grey);
 
 		// Edge filter
-		filter_2d::edge_max<uchar>( grey, edge);
+		switch (filter) {
+			default:
+			case image_filter::edge::OFF:
+				grey.copyTo(edge);
+				break;
+			case image_filter::edge::CANNY:
+				cv::Canny( grey, edge, 30, 100, 3, 	false );
+			break;
+			case image_filter::edge::LAPLACE:
+				cv::Laplacian( grey, edge, 3, 1, 0 );
+			break;
+			case image_filter::edge::MAX:
+				filter_2d::edge_max<uchar>( grey, edge);
+				break;
+
+		}
 
 		// Inverse distance transform
 		filter_2d::inverse_distance_transformation<uchar,uchar>(edge, out);
+	}
+
+	inline void create_blured_image(const cv::Mat& in, cv::Mat &out, image_filter::blur::Blur filter = image_filter::blur::BLUR){
+		switch(filter){
+			default:
+			case ::image_filter::blur::OFF:
+				in.copyTo(out);
+				break;
+			case ::image_filter::blur::BILATERAL:
+				cv::bilateralFilter( in, out, 2, 2*2, 1);
+				break;
+			case ::image_filter::blur::BLUR:
+				cv::blur( in, out, cv::Size(2,2), cv::Point(-1,-1) );
+				break;
+			case ::image_filter::blur::GAUSSIAN:
+				cv::GaussianBlur( in, out, cv::Size(2,2), 0, 0);
+				break;
+			case ::image_filter::blur::MEDIAN:
+				cv::medianBlur( in, out, 2);
+				break;
+		}
 	}
 }
 #endif
